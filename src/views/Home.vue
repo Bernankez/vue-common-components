5a8646f45840e3088650ec509e532dc7c906afd9
shengbeichen <shengbeichen@inspur.com>
2021-01-20 21:45:35 +0800

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,3 +21,12 @@
     computed: {},
   };
 </script>
+
+<style lang="scss" scoped>
+.home_content{
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
