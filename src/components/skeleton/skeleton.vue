d00d610528ace32f4acb1bf1dc2316b95ee564f4
shengbeichen <shengbeichen@inspur.com>
2021-02-02 00:55:36 +0800

    init skeleton

---
+++ src/components/skeleton/skeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "vcc-skeleton",
+    components: {},
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/examples/skeletonPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <vcc-skeleton></vcc-skeleton>
+</template>
+
+<script>
+  import skeleton from "@/components/skeleton/skeleton";
+
+  export default {
+    name: "skeletonPage",
+    components: { "vcc-skeleton": skeleton },
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
